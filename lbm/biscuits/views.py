5b4399c0c51a915b9d644ed9219d0e7498caf37b
Mennad92 <boutaleb.mennad@gmail.com>
2024-07-15 11:40:17 +0200

    Implementation de React

--- lbm/biscuits/views.py
+++ lbm/biscuits/views.py
@@ -1,7 +1,10 @@
 from biscuits.models import *
 from rest_framework import permissions, viewsets
-
 from biscuits.serializers import *
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
 
 class ProduitViewSet(viewsets.ModelViewSet):
     queryset = Produit.objects.all()
--- lbm/lbm/urls.py
+++ lbm/lbm/urls.py
@@ -10,5 +10,6 @@ router.register(r'categories', views.CategorieViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
