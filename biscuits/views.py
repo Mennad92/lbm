337b77971084caedc823c6dda1c6789279813125
Mennad92 <boutaleb.mennad@gmail.com>
2024-08-01 01:22:43 +0200

    WIP implement profile update

--- biscuits/views.py
+++ biscuits/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.views.generic import ListView
 from django.contrib.auth import get_user_model
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -19,8 +19,8 @@ class RegisterView(APIView):
         serializer = UserSerializer(data=self.request.data)
         if serializer.is_valid():
             get_user_model().objects.create_user(**serializer.validated_data)
-            return Response(status=HTTP_201_CREATED)
-        return Response(status=HTTP_400_BAD_REQUEST, data={'errors': serializer.errors})
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={'errors': serializer.errors})
 
 
 class EmailTokenObtainPairView(TokenObtainPairView):
@@ -73,7 +73,19 @@ class ProfileViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def update(self, request, pk=None):
-        raise MethodNotAllowed('PUT')
+        user = request.user
+        data = request.data
+
+        user.first_name = data.get('first_name', user.first_name)
+        user.last_name = data.get('last_name', user.last_name)
+        user.address = data.get('address', user.address)
+        user.city = data.get('city', user.city)
+        user.phone = data.get('phone', user.phone)
+        user.postal = data.get('postal', user.postal)
+        user.save()
+
+        serializer = UserDataSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
         raise MethodNotAllowed('POST')
