d577bba646cb08859419aa4c73d2980176df6c97
Mennad92 <120472084+Mennad92@users.noreply.github.com>
2024-09-25 22:52:39 +0200

    Update settings.py

--- lbm/settings.py
+++ lbm/settings.py
@@ -44,10 +44,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'biscuits.apps.BiscuitsConfig',
 ]
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MIDDLEWARE = [
